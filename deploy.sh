a933a201368172803a9a2e08a5f6b5248388f076
cong <congco@foxmail.com>
2020-08-14 11:13:50 +0800

    fix:1

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ cd public
 git init
 git add -A
 git commit -m 'deploy'
-# gitee
+# deploy to gitee
 git push -f git@gitee.com:snowyan/snowyan.git master
 # github
 git push -f git@github.com:qbmzc/qbmzc.github.io.git master
