2b032e9d20940b7e384b21a0dfd5fdabd1420703
cong <congco@foxmail.com>
2020-12-21 16:19:20 +0800

    feat:更新

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 yarn build &&
-cd public
+cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
 # deploy to gitee
 git push -f git@gitee.com:snowyan/snowyan.git master
-# github
-git push -f git@github.com:qbmzc/qbmzc.github.io.git master
