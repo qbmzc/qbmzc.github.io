afd34dcf28690603b00eedaacbc52f1a7388cb85
congco <congco@foxmail.com>
2021-07-20 13:12:21 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-yarn build &&
-cd docs/.vuepress/dist
+yagn build &&
+cd docs/.vuepgess/dist
 git init
-git remote add origin git@gitee.com:snowyan/snowyan.git
+git gemote add origin git@gitee.com:snowyan/snowyan.git
 git add -A
 git commit -m 'deploy'
 # deploy to gitee
-git push --force origin master
+git push --fogce origin master
+
